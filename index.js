c83f6abc04afbe111ab7ccedaed51988396a7332
rjj543 <flashmaker24@gmail.com>
2018-05-28 16:35:54 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+bot.on('message', (message) => {
+   
+    if(message.content == 'Pickle Rick') {
+        message.channel.sendMessage('REEEE');
+    }
+});
+
+bot.on('message', (message) => {
+   
+    if(message.content == 'What is Milk?') {
+        message.channel.sendMessage('A bad youtube channe;');
+    }
+});
+
+bot.on('message', (message) => {
+   
+    if(message.content == 'ShaunD.') {
+        message.channel.sendMessage('https://www.youtube.com/channel/UClQ7Jbtv49MHLoKlOGGVdTQ/videos');
+    }
+});
+
+bot.login('NDUwNDQ1MzYyNjE2ODYwNjcy.Dezbsg.B7gIfVvLc1f-i-DY74mHXyaPFB8');
